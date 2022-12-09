fd67ccf82b818231f5c2377ee45e1af53909a418
Anastacia <anaanestesia@gmail.com>
2022-12-09 16:27:31 +0000

    module_00 ex00

---
+++ 00_module/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anastacia <anastacia@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 15:24:52 by anastacia         #+#    #+#             */
+/*   Updated: 2022/12/09 16:17:53 by anastacia        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+int	main(int argc, char **argv) {
+	char	ch;
+
+	if (argc == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	for (int j = 1; j < argc; j++){
+		for (int i = 0; i < strlen(argv[j]); i++){
+			ch = (char) toupper(argv[j][i]);
+			std::cout << ch;
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
